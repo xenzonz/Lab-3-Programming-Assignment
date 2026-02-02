2799f2733a925de7956c3e60d1e9d775ad742374
sam cocquyt <samcocquyt04@gmail.com>
2026-02-01 22:49:01 -0500

    docstring added

---
+++ Lab3_xenzonz_replace.py
@@ -0,0 +1,10 @@
+"""
+Docstring for Lab3_xenzonz
+i. LAB 3: Working with a list
+ii. Sam Cocquyt
+iii. Create a list with 10 items, print the count, and print the list alphabetically
+iv. No starter code
+v. 2/1/2026
+"""
+
+print("replace")

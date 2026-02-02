74d259ddcf23df6921f0a066d9ae882724093784
sam cocquyt <samcocquyt04@gmail.com>
2026-02-01 23:33:23 -0500

    logic added, main guard added, slice added

--- Lab3_xenzonz_replace.py
+++ Lab3_xenzonz_replace.py
@@ -2,12 +2,32 @@
 Docstring for Lab3_xenzonz_replace
 i. LAB 3: Working with a list
 ii. Sam Cocquyt
-iii. Create a list with 10 items, print the count, and print the list alphabetically
+iii. Import modified list, replace a middle item with 'binoculars' then print slices
 iv. No starter code
 v. 2/1/2026
 """
 
-print("replace")
-
+# import modified list with 5 new items
 from Lab3_xenzonz_add import camping_items
+
+# middle index
+replace_index = len(camping_items) // 2 
+camping_items[replace_index] = "binoculars"
+
+# binoculars location in index
+binoculars_index = camping_items.index("binoculars")
+
+# main guard
+if __name__ == "__main__":
+    # slice before binoculars
+    print(f"Items before binoculars, not including binoculars:")
+    print(camping_items[:binoculars_index])
+
+    # slice containing only binoculars
+    print(f"Binoculars:")
+    print(camping_items[binoculars_index:binoculars_index + 1])
+
+    #slice after binoculars
+    print(f"Items after binoculars, not including binoculars:")
+    print(camping_items[binoculars_index + 1:])
 
